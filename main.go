b86a33612ce2686a492f527508f07c31a8b9ae20
maeve <maeve.revels@getg5.com>
2018-08-02 20:20:15 -0700

    Simple http server for our baby SPA

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
